a763bd1d6455ec1355fdabc122c7ca917cad40d8
Brian Trapp <brian.trapp@globalfoundries.com>
2020-01-18 12:52:58 -0500

    Need a zero arg constructor for jackson

--- src/main/java/com/btrapp/jklarfreader/objects/KlarfRecord.java
+++ src/main/java/com/btrapp/jklarfreader/objects/KlarfRecord.java
@@ -27,86 +27,97 @@ import java.util.stream.Collectors;
  *
  */
 public final class KlarfRecord {
-  public KlarfRecord(String name, String id) {
-    this.name = name;
-    this.id = id;
-  }
+	//Keep a zero-arg contstructor around so this can object can be created by the Jackson objectmapper
+	@SuppressWarnings("unused")
+	private KlarfRecord() {
 
-  private String name; // ex: FileRecord, LotRecord, WaferRecord...
-  private String id = ""; // 1.8  (Some Records don't have an ID.  Use a blank string for those)
-  private LinkedHashMap<String, List<String>> fields = new LinkedHashMap<>();
-  private List<KlarfList> lists = new ArrayList<>();
-  private List<KlarfRecord> records = new ArrayList<>();
+	}
 
-  /**
-   * A <b>case-insensitive</b> search to match a record by name.
-   *
-   * @param name the name of the record do find (WaferRecord)
-   * @return all matching record objects
-   */
-  public List<KlarfRecord> findRecordsByName(String name) {
-    return records.stream()
-        .filter(r -> name.equalsIgnoreCase(r.getName()))
-        .collect(Collectors.toList());
-  }
+	public KlarfRecord(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
 
-  /**
-   * A <b>case-insensitive</b> search to find a list by name. Multiple records could match, so
-   * return a stream.
-   *
-   * @param name the name of the list to find (DefectList)
-   * @return all matching list objects
-   */
-  public List<KlarfList> findListsByName(String name) {
-    return lists.stream()
-        .filter(r -> name.equalsIgnoreCase(r.getName()))
-        .collect(Collectors.toList());
-  }
+	private String name; // ex: FileRecord, LotRecord, WaferRecord...
+	private String id = ""; // 1.8  (Some Records don't have an ID.  Use a blank string for those)
+	private LinkedHashMap<String, List<String>> fields = new LinkedHashMap<>();
+	private List<KlarfList> lists = new ArrayList<>();
+	private List<KlarfRecord> records = new ArrayList<>();
 
-  /**
-   * A <b>case-insensitive</b> search to match a record by name and an ID
-   *
-   * @param name the name of the record to find (WaferRecord)
-   * @param id the ID of the record to find (MyWafer.01)
-   * @return the record
-   */
-  public Optional<KlarfRecord> findRecordByNameAndId(String name, String id) {
-    return records.stream()
-        .filter(r -> name.equalsIgnoreCase(r.getName()))
-        .filter(r -> id.equalsIgnoreCase(r.getId()))
-        .findFirst();
-  }
+	/**
+	 * A <b>case-insensitive</b> search to match a record by name.
+	 *
+	 * @param name
+	 *            the name of the record do find (WaferRecord)
+	 * @return all matching record objects
+	 */
+	public List<KlarfRecord> findRecordsByName(String name) {
+		return records.stream()
+				.filter(r -> name.equalsIgnoreCase(r.getName()))
+				.collect(Collectors.toList());
+	}
 
-  /**
-   * A <b>case-insensitive</b> search to find a field
-   *
-   * @param name the name of the field to find
-   * @return the list of values
-   */
-  public Optional<List<String>> findField(String name) {
-    return fields.entrySet().stream()
-        .filter(e -> name.equalsIgnoreCase(e.getKey()))
-        .map(e -> e.getValue())
-        .findFirst();
-  }
+	/**
+	 * A <b>case-insensitive</b> search to find a list by name. Multiple records could match, so
+	 * return a stream.
+	 *
+	 * @param name
+	 *            the name of the list to find (DefectList)
+	 * @return all matching list objects
+	 */
+	public List<KlarfList> findListsByName(String name) {
+		return lists.stream()
+				.filter(r -> name.equalsIgnoreCase(r.getName()))
+				.collect(Collectors.toList());
+	}
 
-  public String getName() {
-    return name;
-  }
+	/**
+	 * A <b>case-insensitive</b> search to match a record by name and an ID
+	 *
+	 * @param name
+	 *            the name of the record to find (WaferRecord)
+	 * @param id
+	 *            the ID of the record to find (MyWafer.01)
+	 * @return the record
+	 */
+	public Optional<KlarfRecord> findRecordByNameAndId(String name, String id) {
+		return records.stream()
+				.filter(r -> name.equalsIgnoreCase(r.getName()))
+				.filter(r -> id.equalsIgnoreCase(r.getId()))
+				.findFirst();
+	}
 
-  public String getId() {
-    return id;
-  }
+	/**
+	 * A <b>case-insensitive</b> search to find a field
+	 *
+	 * @param name
+	 *            the name of the field to find
+	 * @return the list of values
+	 */
+	public Optional<List<String>> findField(String name) {
+		return fields.entrySet().stream()
+				.filter(e -> name.equalsIgnoreCase(e.getKey()))
+				.map(e -> e.getValue())
+				.findFirst();
+	}
 
-  public LinkedHashMap<String, List<String>> getFields() {
-    return fields;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public List<KlarfList> getLists() {
-    return lists;
-  }
+	public String getId() {
+		return id;
+	}
 
-  public List<KlarfRecord> getRecords() {
-    return records;
-  }
+	public LinkedHashMap<String, List<String>> getFields() {
+		return fields;
+	}
+
+	public List<KlarfList> getLists() {
+		return lists;
+	}
+
+	public List<KlarfRecord> getRecords() {
+		return records;
+	}
 }
