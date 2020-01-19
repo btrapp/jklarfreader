d82820ee2c491f8ed69e62e77b369017d0c07069
Brian Trapp <brian.trapp@globalfoundries.com>
2020-01-19 08:57:55 -0500

    Add some methods to KlarfRecord that help get data out with some business logic around em

---
+++ src/main/java/com/btrapp/jklarfreader/objects/KlarfContentException.java
@@ -0,0 +1,16 @@
+package com.btrapp.jklarfreader.objects;
+
+/**
+ * An exception for when the content of the Klarf doesn't match the pattern your business logic expects
+ * (ex: getField(x) is an array of 3 but you only allow an array of 2)
+ * Mostly thrown by the reqField(...) methods
+ *
+ * @author btrapp
+ */
+public class KlarfContentException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public KlarfContentException(String msg) {
+		super(msg);
+	}
+}
--- src/main/java/com/btrapp/jklarfreader/objects/KlarfException.java
+++ src/main/java/com/btrapp/jklarfreader/objects/KlarfException.java
@@ -1,36 +1,39 @@
 package com.btrapp.jklarfreader.objects;
 
 /**
- * An exception for when something's wrong with the Klarf format
+ * An exception for when something's wrong with the Klarf format itself (like a list length is wrong)
+ * 
+ * @See KlarfContentException for when the format is correct but the contents don't follow a business logic rule
+ *      (like if you say ResultTimestamp is expected to be a String of 2 items, but only 1 is present)
  *
  * @author btrapp
  */
 public class KlarfException extends Exception {
-  private static final long serialVersionUID = -6451200672730727155L;
+	private static final long serialVersionUID = -6451200672730727155L;
 
-  public enum ExceptionCode {
-    GenericError,
-    ListFormat,
-    NumberFormat,
-    UnsupportedKlarfVersion
-  }
+	public enum ExceptionCode {
+		GenericError,
+		ListFormat,
+		NumberFormat,
+		UnsupportedKlarfVersion
+	}
 
-  private ExceptionCode code = ExceptionCode.GenericError;
+	private ExceptionCode code = ExceptionCode.GenericError;
 
-  public KlarfException(String msg) {
-    this(msg, null, ExceptionCode.GenericError);
-  }
+	public KlarfException(String msg) {
+		this(msg, null, ExceptionCode.GenericError);
+	}
 
-  public KlarfException(String msg, KlarfTokenizer kt, ExceptionCode code) {
-    super(
-        msg
-            + ((kt == null)
-                ? ""
-                : " (At line " + kt.getLineNumber() + ": " + kt.getCurrentLine() + ")"));
-    this.code = code;
-  }
+	public KlarfException(String msg, KlarfTokenizer kt, ExceptionCode code) {
+		super(
+				msg
+						+ ((kt == null)
+								? ""
+								: " (At line " + kt.getLineNumber() + ": " + kt.getCurrentLine() + ")"));
+		this.code = code;
+	}
 
-  public ExceptionCode getCode() {
-    return code;
-  }
+	public ExceptionCode getCode() {
+		return code;
+	}
 }
