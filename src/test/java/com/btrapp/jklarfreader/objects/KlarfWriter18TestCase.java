e76f4da720b4572f3b68da3ae436e8c71226e83c
Brian Trapp <brian.trapp@globalfoundries.com>
2022-02-01 12:02:32 -0500

    Fix the bug in the id printing and test it

---
+++ src/test/java/com/btrapp/jklarfreader/objects/KlarfWriter18TestCase.java
@@ -0,0 +1,33 @@
+package com.btrapp.jklarfreader.objects;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.junit.jupiter.api.Test;
+
+public class KlarfWriter18TestCase {
+	@Test
+	public void testKlarfRecordIdWriting() throws IOException {
+
+		KlarfRecord krWithId = new KlarfRecord("MyName", "MyId");
+
+		KlarfWriter18 kw = new KlarfWriter18();
+		StringWriter sw = new StringWriter();
+		kw.writeKlarf(krWithId, sw);
+		assertTrue(sw.toString().contains("Record MyName \"MyId\""));
+	}
+
+	@Test
+	public void testKlarfRecordNoIdWriting() throws IOException {
+
+		KlarfRecord krWithId = new KlarfRecord("MyName", "");
+
+		KlarfWriter18 kw = new KlarfWriter18();
+		StringWriter sw = new StringWriter();
+		kw.writeKlarf(krWithId, sw);
+		//System.out.println(sw.toString());
+		assertTrue(sw.toString().contains("Record MyName {")); //Be sure we're not writing a blank id of Record MyName "" {
+	}
+}
