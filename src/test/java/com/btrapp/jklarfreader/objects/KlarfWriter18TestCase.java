0b2bd9dce25470ed5bb158087a368b08e39d0c36
Brian Trapp <brian.trapp@globalfoundries.com>
2022-02-02 19:48:35 -0500

    Formatting

--- src/test/java/com/btrapp/jklarfreader/objects/KlarfWriter18TestCase.java
+++ src/test/java/com/btrapp/jklarfreader/objects/KlarfWriter18TestCase.java
@@ -4,30 +4,32 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.io.StringWriter;
-
 import org.junit.jupiter.api.Test;
 
 public class KlarfWriter18TestCase {
-	@Test
-	public void testKlarfRecordIdWriting() throws IOException {
+  @Test
+  public void testKlarfRecordIdWriting() throws IOException {
 
-		KlarfRecord krWithId = new KlarfRecord("MyName", "MyId");
+    KlarfRecord krWithId = new KlarfRecord("MyName", "MyId");
 
-		KlarfWriter18 kw = new KlarfWriter18();
-		StringWriter sw = new StringWriter();
-		kw.writeKlarf(krWithId, sw);
-		assertTrue(sw.toString().contains("Record MyName \"MyId\""));
-	}
+    KlarfWriter18 kw = new KlarfWriter18();
+    StringWriter sw = new StringWriter();
+    kw.writeKlarf(krWithId, sw);
+    assertTrue(sw.toString().contains("Record MyName \"MyId\""));
+  }
 
-	@Test
-	public void testKlarfRecordNoIdWriting() throws IOException {
+  @Test
+  public void testKlarfRecordNoIdWriting() throws IOException {
 
-		KlarfRecord krWithId = new KlarfRecord("MyName", "");
+    KlarfRecord krWithId = new KlarfRecord("MyName", "");
 
-		KlarfWriter18 kw = new KlarfWriter18();
-		StringWriter sw = new StringWriter();
-		kw.writeKlarf(krWithId, sw);
-		//System.out.println(sw.toString());
-		assertTrue(sw.toString().contains("Record MyName {")); //Be sure we're not writing a blank id of Record MyName "" {
-	}
+    KlarfWriter18 kw = new KlarfWriter18();
+    StringWriter sw = new StringWriter();
+    kw.writeKlarf(krWithId, sw);
+    // System.out.println(sw.toString());
+    assertTrue(
+        sw.toString()
+            .contains(
+                "Record MyName {")); // Be sure we're not writing a blank id of Record MyName "" {
+  }
 }
