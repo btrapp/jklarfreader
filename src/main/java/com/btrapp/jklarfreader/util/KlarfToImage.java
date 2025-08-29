bc1d81def8909470817463c0ea73a86839086c05
Brian Trapp <brian.trapp@globalfoundries.com>
2025-08-29 09:25:48 -0400

    Add image method that accepts a klarfrecord directly

--- src/main/java/com/btrapp/jklarfreader/util/KlarfToImage.java
+++ src/main/java/com/btrapp/jklarfreader/util/KlarfToImage.java
@@ -398,13 +398,18 @@ public class KlarfToImage {
     }
   }
 
-  public static void drawKlarf(KlarfImageOptions kio, BufferedInputStream bis) {
+  public static void drawKlarf(KlarfImageOptions kio, BufferedInputStream bis) throws Exception{
+	  KlarfRecord klarf = KlarfReader.parseKlarf(bis).orElse(null);
+	  if (klarf == null) {
+	        System.err.println("Unable to read klarf");
+	        System.exit(1);
+	  }
+	  drawKlarf(kio,klarf);
+  }
+
+  public static void drawKlarf(KlarfImageOptions kio, KlarfRecord klarf) {
     try {
-      KlarfRecord klarf = KlarfReader.parseKlarf(bis).orElse(null);
-      if (klarf == null) {
-        System.err.println("Unable to read klarf");
-        System.exit(1);
-      }
+      
       KlarfRecord lotRec = klarf.findRecordsByName("LotRecord").stream().findFirst().orElseThrow();
       KlarfRecord waferRec =
           lotRec.findRecordsByName("WaferRecord").stream().findFirst().orElseThrow();
